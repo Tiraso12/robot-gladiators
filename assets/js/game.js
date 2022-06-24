37a1081dc3d715ce9c1e5720d8fac4b0026b5b06
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-06-24 13:00:17 -0400

    extensive fight'

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,9 +8,6 @@ var playerMoney = 10;
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
 
 for (var i = 0; i< enemyNames.length; i++){
   console.log(enemyNames[i]);
@@ -18,8 +15,10 @@ for (var i = 0; i< enemyNames.length; i++){
   console.log
 }
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+
+  //repeat and execute as long as the enemy-robot is alive
+  while (enemyHealth > 0) {
+    // place fight function code block here...
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -71,8 +70,12 @@ var fight = function(enemyName) {
     window.alert("You need to pick a valid option. Try again!");
   }
 }; // end of fight function
+  }
+  
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
   fight(enemyNames[i]);
 }
