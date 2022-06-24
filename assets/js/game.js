4438ff993f01b0e30b6287d4cfe2ab5d5c1c9557
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-06-24 12:05:39 -0400

    fixed error

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,20 +6,17 @@ var playerMoney = 10;
 
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]); 
-console.log(enemyNames.length); 
-
-for ( var i = 0; i < enemyNames.length; i++){
-  console.log(enemyNames[i]);
-  console.log(i);
-  console.log(enemyNames[i] + "is at" +i+ " index");
-}
 var enemyHealth = 50;
 var enemyAttack = 12;
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
 
-// fight function
+for (var i = 0; i< enemyNames.length; i++){
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index")
+}
 var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
@@ -76,7 +73,4 @@ var fight = function(enemyName) {
 }; // end of fight function
 
 // run fight function to start game
-fight();
-for (var i =0; i<enemyNames.length;i++){
-  fight(enemyNames[i]);
-}
+//fight();
